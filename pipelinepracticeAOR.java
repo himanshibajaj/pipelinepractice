572c97c94aacc70de6e5bfae68ac2431b7533aee
Himanshi Bajaj <himanshibajaj723@gmail.com>
2024-10-16 16:15:31 +0530

    Create pipelinepracticeAOR.java

---
+++ pipelinepracticeAOR.java
@@ -0,0 +1,11 @@
+// Java Program to Calculate Area of a Rectangle
+public class pipelinepracticeAOR {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 10;
+
+        int area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
